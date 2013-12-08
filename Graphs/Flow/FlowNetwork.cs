88b3781f36034d3d0ee25359bb92034f99cc9849
Eyas <eyas.sharaiha@gmail.com>
2013-12-08 14:01:52 -0500

    FlowNetwork skeleton

--- Graphs/Flow/FlowNetwork.cs
+++ Graphs/Flow/FlowNetwork.cs
@@ -4,5 +4,20 @@ namespace Graphs.Flow
 {
     class FlowNetwork
     {
+        public Vertex s
+        {
+            private set;
+            public get;
+        }
+        public Vertex t
+        {
+            private set;
+            public get;
+        }
+        private IGraph _graph;
+        public FlowNetwork(IGraph graph, Vertex source, Vertex target)
+        {
+
+        }
     }
 }
