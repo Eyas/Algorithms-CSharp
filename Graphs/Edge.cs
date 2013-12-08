57b960b9fac93282145cad94f1d8c7794e3a835b
Eyas <eyas.sharaiha@gmail.com>
2013-12-08 13:56:24 -0500

    added Edge implementation.

---
+++ Graphs/Edge.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graphs
+{
+    public class Edge
+    {
+        public readonly Vertex u;
+        public readonly Vertex v;
+        public readonly int? weight;
+        public Edge(Vertex u, Vertex v)
+        {
+            this.u = u;
+            this.v = v;
+        }
+        public Edge(Vertex u, Vertex v, int weight)
+        {
+            this.weight = weight;
+            this.u = u;
+            this.v = v;
+        }
+        public override bool Equals(object obj)
+        {
+            Edge other = obj as Edge;
+            if (other == null) return false;
+            return u.Equals(other.u) && v.Equals(other.v);
+        }
+        public override int GetHashCode()
+        {
+            return u.GetHashCode() ^ v.GetHashCode();
+        }
+        public override string ToString()
+        {
+            return new StringBuilder().Append("Edge(").Append(u).Append(",").Append(")").ToString();
+        }
+    }
+}
