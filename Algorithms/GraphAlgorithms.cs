9edd0c1fa3bc3d9145736af1739ec8e9bea85a8c
Eyas <eyas.sharaiha@gmail.com>
2015-01-03 15:53:48 -0500

    Making topological sort more efficient, and annotating it with notes about its time coplexity.

--- Algorithms/GraphAlgorithms.cs
+++ Algorithms/GraphAlgorithms.cs
@@ -20,32 +20,49 @@ namespace Algorithms
             }
 
         }
+        /// <summary>
+        /// Returns a topological sort of a given graph.
+        /// </summary>
+        /// <remarks>
+        /// Overall complexity: O(|V|^2) for adjacency matrices, and
+        /// O(|V|+|E|) for adjacency lists.
+        /// </remarks>
         public static IEnumerable<Vertex> TopologicalSort(
             this IIncidentsGraph graph)
         {
-            // TODO: use min-heap here
+         
+            // O(|V|^2)   for Adjacency Matrix,
+            // O(|V|)     for Adjacency-Incident Set with cached lengths
+            // O(|V|+|E|) for Incidence Set w/o cached lengths
+            // O(|V|+|E|) for gradual buildup
             Dictionary<Vertex, int> indeg = graph.Vertices()
-                .ToDictionary(v => { return v; }, v => { return graph.Incidents(v).Count(); });
+                .ToDictionary(v => { return v; }, v => { return graph.InDegree(v); });
 
+            // O(|V|)
+            Queue<Vertex> toExplore =
+                new Queue<Vertex>(indeg.Where(kv => { return kv.Value == 0; }).Select(x => { return x.Key; }));
+
+            // iterated |V| times; indeg removed from once in each iteration
             while (indeg.Count > 0)
             {
-                if (indeg.Any(kv => { return kv.Value == 0; }) == false)
+                if (toExplore.Count == 0)
                     throw new Exception("Cycles detected");
 
-                IEnumerable<Vertex> candidates = indeg.Where(kv => { return kv.Value == 0; }).Select(x => { return x.Key; });
+                Vertex v = toExplore.Dequeue();
+                yield return v;
 
-                foreach (Vertex candidate in candidates)
+                indeg.Remove(v);
+                // either O(|V|) for adjacency matrix, of
+                //     or O(|E|) over all iterations for adjacency list
+                foreach (Vertex dest in graph.Neighbors(v))
                 {
-                    indeg.Remove(candidate);
-                    foreach (Vertex dest in graph.Neighbors(candidate))
+                    if (--indeg[dest] == 0)
                     {
-                        indeg[dest]--;
+                        toExplore.Enqueue(dest);
                     }
-
-                    yield return candidate;
                 }
 
-            }
+            } // loop is either O(|V|^2) for adjacency matrix, of O(|V|+|E|) for adjacency list
 
         }
     }
